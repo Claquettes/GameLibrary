f693a4390c88ddbb98b1c18ac6c6d22c40c4cdf7
Claquettes <math@claq.fr>
2023-09-06 21:38:47 +0200

    impotred bootstrap, created a first component with a card and props

--- gamelib/src/App.tsx
+++ gamelib/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+//we import all of the components
+import GameCard from './components/GameCard';
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -13,12 +13,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <GameCard title="Game Title" description="Game Description" image="https://via.placeholder.com/150" link="https://via.placeholder.com/150" />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ gamelib/src/components/GameCard.tsx
@@ -0,0 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+interface GameCardProps {
+    title: string;
+    description: string;
+    image: string;
+    link: string;
+}
+
+function GameCard(props: GameCardProps) {
+  
+  //to build them dynamically, we use props
+  const { title, description, image, link } = props;
+  return (
+    <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://via.placeholder.com/150" />
+        <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>
+            {description}
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+    </Card>
+        
+  );
+}
+
+export default GameCard ;
